664896c090b76b8c7642515c473e41bd17f1eb54
nico-solution <nicochen0206@gmail.com>
2023-06-06 18:12:27 +0600

    feat add chrome driver

--- update-multi.py
+++ update-multi.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from random import randint
 import time
+import re
 from datetime import datetime
 from auth import spreadsheet_service
 from selenium import webdriver
@@ -22,68 +23,96 @@ chrome_options.add_argument(
 chrome_options.add_argument('--profile-directory=Profile 1')
 
 driver = webdriver.Chrome(options=chrome_options)
-from_row_number=7
-to_row_number=12
+from_row_number=22
+to_row_number=100
 # driver = uc.Chrome()
 values = []
 
 
-def scrap_shopee(driver,variation_product):
-    variationElement = driver.find_elements(By.CLASS_NAME, "product-variation")
+def scrap_shopee(url,driver,variation_product,before_URL):
     variationArray=[]
+    if before_URL !=url:  
+        driver.get(url)
+        wait = WebDriverWait(driver, 30)
+    variationElement = driver.find_elements(By.CLASS_NAME, "product-variation")
     if len(variationElement) > 0:
         for element in variationElement:
             if variation_product[0] == element.text:
                 
                 element.click()
-                time.sleep(randint(8,10))
+                # wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'pqTWkA')))
                 priceElement = driver.find_elements(By.CLASS_NAME, "pqTWkA")
                 price = priceElement[0].text
+                # wait.until(EC.presence_of_element_located((By.CLASS_NAME, '_44qnta')))
                 productTitleElement = driver.find_elements(By.CLASS_NAME, "_44qnta")
-                productTitle=productTitleElement[-1].find_element(By.XPATH, "./span[last()]").text
-                moqElement = driver.find_elements(By.CLASS_NAME, "I+H1Co")
-                if len(moqElement)>0 :
-                    moqText=moqElement[0].text
-                    moq = [int(s) for s in moqText.split() if s.isdigit()][0]
-                else:
-                    moq=""
-            
+                productTitle=productTitleElement[-1].find_element(By.XPATH, ".//span[last()]").text
+                # moqElement = driver.find_elements(By.CLASS_NAME, "I+H1Co")
+                # print(moqElement,"moqtext")
+                # if len(moqElement)>0 :
+                #     moqText=moqElement[0].text
+                #     # moq = [int(s) for s in moqText.split() if s.isdigit()][0]
+                #     moq = re.findall(r'\d+', moqText)[0] 
+
+                # else:
+                #     moq=""
+
+                elements = driver.find_elements(By.TAG_NAME,"div")
+                moq=""
+                for element in elements:
+                    if "This item has a Minimum Purchase Quantity (MPQ) requirement of" in element.text:
+                        moq=re.findall(r'\d+', element.text)[0]
+                        break
+                # wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.flex.items-center._6lioXX > div:last-child')))
+                
                 piece_available_element = driver.find_elements(By.CSS_SELECTOR, ".flex.items-center._6lioXX > div:last-child")[0].text
-                print(piece_available_element)
-                time.sleep(randint(2,4))
-                stock=[int(s) for s in piece_available_element.split() if s.isdigit()][0]
+                
+                # stock=[int(s) for s in piece_available_element.split() if s.isdigit()][0]
+                stock = re.findall(r'\d+', piece_available_element)[0] 
                 variation=variation_product[0]
                 now = datetime.now()
 
-                current_time = now.strftime("%Y-%m-%d-%H-%M")
-                return ([productTitle,variation,price, stock, moq,current_time]) 
+                current_time = now.strftime("%H:%M %m-%d-%Y")
+                return ([productTitle,variation,price, stock, moq,current_time])
+             
     else: 
             variationArray=[]
+            # wait.until(EC.presence_of_element_located((By.CLASS_NAME, '_44qnta')))
             productTitleElement = driver.find_elements(By.CLASS_NAME, "_44qnta")
-            productTitle=productTitleElement[-1].find_element(By.XPATH, "./span[last()]").text
+            productTitle=productTitleElement[-1].find_element(By.XPATH, ".//span[last()]").text
+            # wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'pqTWkA')))
             priceElement = driver.find_elements(By.CLASS_NAME, "pqTWkA")
             price = priceElement[0].text
-            moqElement = driver.find_elements(By.CLASS_NAME, "I+H1Co")
-            if len(moqElement)>0 :
-                moqText=moqElement[0].text
-                moq = [int(s) for s in moqText.split() if s.isdigit()][0]
-            else:
-                moq=""
-        
+            # moqElement = driver.find_elements(By.CLASS_NAME, "I+H1Co")
+            # if len(moqElement)>0 :
+            #     moqText=moqElement[0].text
+            #     # moq = [int(s) for s in moqText.split() if s.isdigit()][0]
+            #     moq = re.findall(r'\d+', moqText)[0]
+            # else:
+            #     moq=""
+
+            elements = driver.find_elements(By.TAG_NAME,"div")
+            moq=""
+            for element in elements:
+                if "This item has a Minimum Purchase Quantity (MPQ) requirement of" in element.text:
+                    moq=re.findall(r'\d+', element.text)[0]
+                    break
+            # wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.flex.items-center._6lioXX > div:last-child')))
+
             piece_available_element = driver.find_elements(By.CSS_SELECTOR, ".flex.items-center._6lioXX > div:last-child")[0].text
          
             
-            stock=[int(s) for s in piece_available_element.split() if s.isdigit()][0]
+            # stock=[int(s) for s in piece_available_element.split() if s.isdigit()][0]
+            stock = re.findall(r'\d+', piece_available_element)[0] 
             variation=' '.join(variationArray)
             now = datetime.now()
 
-            current_time = now.strftime("%Y-%m-%d-%H-%M")
+            current_time = now.strftime("%H:%M %m-%d-%Y")
             return([productTitle,variation,price, stock, moq,current_time])
     
 
 def read_range():
 
-    range_name = 'Sheet1!D{}:D{}'  # read an empty row for new data
+    range_name = 'Sheet1!D{}:D{}' 
     variationRange_name='Sheet1!C{}:C{}'
     spreadsheet_id = '17IaVsJOqLdBtT3UYChbOdr0pPXr7nUT43T0Qs1ck3nU'
 
@@ -97,44 +126,20 @@ def read_range():
         print(url)
         if url != []:
             if url[0].find('seller') == -1 and url[0].find("product") == -1:
-                    driver.get(url[0])
-                    time.sleep(randint(10,15))
-                    
-                    # elem = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.product-briefing")))
-                    result=scrap_shopee(driver,variations[index])
-                    print(result,url)
-                    # values.append(result)
+                    result=scrap_shopee(url[0],driver,variations[index],rows[index-1][0])
                     write_range(from_row_number+index,result) 
-                            
-
             elif url[0].find('seller') == -1 and url[0].find("product") >= 0:
-                    driver.get(url[0])
-                    time.sleep(randint(10,15))
-                   
-                    # elem = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.product-briefing")))
-                    result=scrap_shopee(driver,variations[index])
-                    print(result,url)
+                    result=scrap_shopee(url[0],driver,variations[index],rows[index-1][0])
                     write_range(from_row_number+index,result) 
-
-            else:        # values.append(result)  
+            else:         
                 now = datetime.now()
-                current_time = now.strftime("%Y-%m-%d-%H-%M")
+                current_time = now.strftime("%H:%M %m-%d-%Y")
                 write_range(from_row_number+index,["", "", "","","",current_time])
-
-                            
-
         else:
             now = datetime.now()
-
-            current_time = now.strftime("%Y-%m-%d-%H-%M")
+            current_time = now.strftime("%H:%M %m-%d-%Y")
             write_range(from_row_number+index,["", "", "","","",current_time]) 
-            # values.append(["", "", "","","",current_time])
-
-    # print('{0} rows retrieved.'.format(len(rows)))
-
-    # print(priceElement)
-
-    # print('{0} rows retrieved.'.format(rows))
+          
     driver.close()
     return
 
